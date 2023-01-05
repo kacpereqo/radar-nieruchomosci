193248828f53c587651d57b84a26a2eab2829f80
kacpereqqo <remzak.k@gmail.com>
2023-01-06 00:25:01 +0100

    created basic data extraction functionality

--- app/scrapper/schemas.py
+++ app/scrapper/schemas.py
@@ -1,11 +1,15 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Flat(BaseModel):
-    title: str
     price: int
-    area: float
+    area: int
     rooms: int
     floor: int
     is_rent: bool
+    is_furnished: Optional[bool]
+    type_of_building: str
+    owner: str
+    url: str
     price_per_sqmeter: int
--- app/scrapper/service.py
+++ app/scrapper/service.py
@@ -10,12 +10,12 @@ class ScrapperService:
     def __init__(self, url):
         self.url = url
 
-    def get_html(self) -> str:
+    def get_page_html(self) -> str:
         r = requests.get(self.url)
         return r.text
 
     def get_flats_urls(self) -> Dict[str, List[str]]:
-        html = self.get_html()
+        html = self.get_page_html()
         soup = BeautifulSoup(html, 'html.parser')
         flats = soup.find_all('a', class_='css-rc5s2u')
 
@@ -33,17 +33,70 @@ class ScrapperService:
             elif href[0] == 'h':
                 flat_urls['otodom'].append(href)
 
-        return flat_urls
+        self.flat_urls = flat_urls
 
-    # def get_olx_flat_details(self, url: str) -> Type[Flat]:
-    #     html = self.get_html()
-    #     soup = BeautifulSoup(html, 'html.parser')
+    def get_olx_flat_details(self, url: str) -> List[str]:
+        html = requests.get(url).text
+        soup = BeautifulSoup(html, 'html.parser')
 
-    #     title = soup.find('h1', class_='css-1jy2b5v').text
-    #     price = soup.find('strong', class_='css-1w6kz
+        flat_data = soup.find_all('li', class_='css-ox1ptj')
+        flat_data = [data.text for data in flat_data]
+        flat_data.append(
+            soup.find('h3', class_='css-19cr6mc-TextStyled er34gjf0').text)
+        flat_data.append(url)
+
+        return Flat(**self.filter_data_from_olx(flat_data))
+
+    def filter_data_from_olx(self, flat_data: str) -> Dict[str, str]:
+        data = {}
+
+        data['owner'] = flat_data[0]
+        data['price'] = int(flat_data[-2].replace('zł', '').replace(' ', ''))
+        data['url'] = self.url + flat_data[-1]
+
+        for i in flat_data[1:-2]:
+            print(i)
+            category, value = i.split(': ')
+
+            if category == 'Cena':
+                data['price'] = int(value)
+
+            elif category == 'Powierzchnia':
+                value = value.replace(' m²', '')
+                data['area'] = int(value)
+
+            elif category == 'Liczba pokoi':
+                if value == 'Kawalerka':
+                    value = 1
+                else:
+                    value = value.split(' ')[0]
+                data['rooms'] = int(value)
+
+            elif category == 'Poziom':
+                if value == 'Parter':
+                    value = 0
+                data['floor'] = int(value)
+
+            elif category == 'Rodzaj zabudowy':
+                data['type_of_building'] = value
+
+            elif category == 'Czynsz':
+                data['rent'] = int(value)
+
+        data['price_per_sqmeter'] = round(data['price'] / data['area'], 2)
+        data['is_rent'] = True if self.url.split(
+            '/')[-2] == 'wynajem' else False
+
+        print(data)
+        return data
+
+    def get_Flats(self) -> List[Type[Flat]]:
+        for url in self.flat_urls['olx']:
+            self.get_olx_flat_details(url)
 
 
 scrapper = ScrapperService(
-    'https://www.olx.pl/d/nieruchomosci/mieszkania/')
+    'https://www.olx.pl/d/nieruchomosci/mieszkania/wynajem/')
 
 scrapper.get_flats_urls()
+scrapper.get_Flats()
